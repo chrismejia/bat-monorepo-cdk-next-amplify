0be4c341533c3cb0bb1e2931f8723edd6739d647
Christian Mejia <Christian.Mejia@hearst.com>
2023-11-09 14:17:06 -0500

    move backend, frontend into packages

--- packages/backend/bin/backend.ts
+++ packages/backend/bin/backend.ts
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { AuthStack } from "./lib/AuthStack";
-import { IdentityStack } from "./lib/IdentityStack";
-import { AmplifyHostingStack } from "./lib/NextJSHostingStack";
+import { AuthStack } from "../lib/AuthStack";
+import { IdentityStack } from "../lib/IdentityStack";
+import { AmplifyHostingStack } from "../lib/NextJSHostingStack";
 
 const app = new cdk.App();
 
-const authStack = new AuthStack(app, "ProductAuthStack", {});
+const authStack = new AuthStack(app, "BATAuthStack", {});
 
-const identityStack = new IdentityStack(app, "ProductIdentityStack", {
+const identityStack = new IdentityStack(app, "BATProductIdentityStack", {
   userpool: authStack.userpool,
   userpoolClient: authStack.userPoolClient,
 });
 
 const amplifyHostingStack = new AmplifyHostingStack(
   app,
-  "ProductHostingStack",
+  "BATProductHostingStack",
   {
     //pass in any envVars from the above stacks here
     environmentVariables: {
       USERPOOL_ID: authStack.userpool.userPoolId,
-      AMPLIFY_MONOREPO_APP_ROOT: "frontend",
+      AMPLIFY_MONOREPO_APP_ROOT: "packages/frontend",
     },
   }
 );
--- packages/backend/lib/NextJSHostingStack.ts
+++ packages/backend/lib/NextJSHostingStack.ts
@@ -1,34 +1,30 @@
-import { CfnOutput, SecretValue, Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as codebuild from "aws-cdk-lib/aws-codebuild";
-import {
-  App,
-  GitHubSourceCodeProvider,
-  RedirectStatus,
-} from "@aws-cdk/aws-amplify-alpha";
+import { App, RedirectStatus } from "@aws-cdk/aws-amplify-alpha";
 import { CfnApp } from "aws-cdk-lib/aws-amplify";
 
 interface HostingStackProps extends StackProps {
-
   readonly environmentVariables?: { [name: string]: string };
 }
 
 export class AmplifyHostingStack extends Stack {
   constructor(scope: Construct, id: string, props: HostingStackProps) {
     super(scope, id, props);
-    const amplifyApp = new App(this, "BATNextSample", {
-      appName: "BAT Next Via Amplify through AWS CDK",
+    const amplifyApp = new App(this, "BATMonorepoCDKNextFrontend", {
+      appName: "BAT Monorepo with CDK + Next Frontend",
       autoBranchDeletion: true,
       customRules: [
         {
           source: "/<*>",
-          target: "	/index.html",
+          target: " /index.html",
           status: RedirectStatus.NOT_FOUND_REWRITE,
         },
       ],
       environmentVariables: props.environmentVariables,
       buildSpec: codebuild.BuildSpec.fromObjectToYaml({
         version: 1,
+        appRoot: "packages/frontend",
         frontend: {
           phases: {
             preBuild: {
