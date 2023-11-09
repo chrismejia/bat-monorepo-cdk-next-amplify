247863d1d468d74053ac9e81c9efa12e0d0c27b0
Christian Mejia <Christian.Mejia@hearst.com>
2023-11-09 15:49:53 -0500

    update Next front page detailing monorepo

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,36 +1,38 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { Auth } from 'aws-amplify';
-import Link from 'next/link';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { Auth } from "aws-amplify";
+import Link from "next/link";
 
 const Home = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(() => {
-        const redirectToDashboardIfLoggedIn = async () => {
-            try {
-                await Auth.currentAuthenticatedUser();
-                // If user is authenticated, redirect to the dashboard
-                router.push('/dashboard');
-            } catch {
-                // If there's an error, it likely means no user is signed in
-                // Stay on the home page
-            }
-        };
-
-        redirectToDashboardIfLoggedIn();
-    }, [router]);
-
-    const handleLoginClick = () => {
-        router.push('/login');
+  useEffect(() => {
+    const redirectToDashboardIfLoggedIn = async () => {
+      try {
+        await Auth.currentAuthenticatedUser();
+        // If user is authenticated, redirect to the dashboard
+        router.push("/dashboard");
+      } catch {
+        // If there's an error, it likely means no user is signed in
+        // Stay on the home page
+      }
     };
 
-    return (
-        <div>
-            <h1>Welcome to the App</h1>
-            <button onClick={handleLoginClick}>Log In</button>
-        </div>
-    );
+    redirectToDashboardIfLoggedIn();
+  }, [router]);
+
+  const handleLoginClick = () => {
+    router.push("/login");
+  };
+
+  return (
+    <div>
+      <h1>Welcome to the App</h1>
+      <p>This is a monorepo with packages for CDK stacks + NextJS frontend.</p>
+      <p>Entirely deployed via CDK.</p>
+      <button onClick={handleLoginClick}>Log In</button>
+    </div>
+  );
 };
 
 export default Home;
